3c11d7955c6e80075f6ed740871ef0a36ce16b38
Danilo Lima <danilolimas.pe@gmail.com>
2021-12-14 00:42:11 -0300

    Adiçao previa da VIEW da listagem de jogos com capas

--- application/views/usuario/home.php
+++ application/views/usuario/home.php
@@ -78,5 +78,144 @@ if($this->session->userdata('perfil') != 2){
 
     </div> -->
 
+        <div class="">
+        <div class="page-title">
+            <div class="title_left">
+            <h3> Meus Jogos <small> </small> </h3>
+            </div>
+
+            <div class="title_right">
+            <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                <div class="input-group">
+                <input type="text" class="form-control" placeholder="Pesquisar...">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">Buscar</button>
+                </span>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-12">
+            <div class="x_panel">
+                <div class="x_title">
+                <h2>Listagem </h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+
+                </ul>
+                <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                <div class="row">
+
+                    <div class="col-md-55">
+                    <div class="thumbnail">
+                        <div class="image view view-first">
+                        <img style="width: 100%; display: block;" src="<?= BASE_URL()?>assets/imagens/media.jpg" alt="image" />
+                        <div class="mask no-caption">
+                            <div class="tools tools-bottom">
+                            <a href="#"><i class="fa fa-link"></i></a>
+                            <a href="#"><i class="fa fa-pencil"></i></a>
+                            <a href="#"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="caption">
+                        <p><strong>Título Jogo</strong>
+                        </p>
+                        <p>Nota</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-55">
+                    <div class="thumbnail">
+                        <div class="image view view-first">
+                        <img style="width: 100%; display: block;" src="<?= BASE_URL()?>assets/imagens/media.jpg" alt="image" />
+                        <div class="mask no-caption">
+                            <div class="tools tools-bottom">
+                            <a href="#"><i class="fa fa-link"></i></a>
+                            <a href="#"><i class="fa fa-pencil"></i></a>
+                            <a href="#"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="caption">
+                        <p><strong>Título Jogo</strong>
+                        </p>
+                        <p>Nota</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-55">
+                    <div class="thumbnail">
+                        <div class="image view view-first">
+                        <img style="width: 100%; display: block;" src="<?= BASE_URL()?>assets/imagens/media.jpg" alt="image" />
+                        <div class="mask no-caption">
+                            <div class="tools tools-bottom">
+                            <a href="#"><i class="fa fa-link"></i></a>
+                            <a href="#"><i class="fa fa-pencil"></i></a>
+                            <a href="#"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="caption">
+                        <p><strong>Título Jogo</strong>
+                        </p>
+                        <p>Nota</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-55">
+                    <div class="thumbnail">
+                        <div class="image view view-first">
+                        <img style="width: 100%; display: block;" src="<?= BASE_URL()?>assets/imagens/media.jpg" alt="image" />
+                        <div class="mask no-caption">
+                            <div class="tools tools-bottom">
+                            <a href="#"><i class="fa fa-link"></i></a>
+                            <a href="#"><i class="fa fa-pencil"></i></a>
+                            <a href="#"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="caption">
+                        <p><strong>Título Jogo</strong>
+                        </p>
+                        <p>Nota</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-55">
+                    <div class="thumbnail">
+                        <div class="image view view-first">
+                        <img style="width: 100%; display: block;" src="<?= BASE_URL()?>assets/imagens/media.jpg" alt="image" />
+                        <div class="mask no-caption">
+                            <div class="tools tools-bottom">
+                            <a href="#"><i class="fa fa-link"></i></a>
+                            <a href="#"><i class="fa fa-pencil"></i></a>
+                            <a href="#"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="caption">
+                        <p><strong>Título Jogo</strong>
+                        </p>
+                        <p>Nota</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+
 </div>
 <!-- /page content -->
+
